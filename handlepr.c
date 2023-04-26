62e56cd95d6738eb40c6db707923f26d4d2ab0ae
AmineCoder30 <code30a@gmail.com>
2023-04-26 15:22:39 +0200

    h

--- handlepr.c
+++ handlepr.c
@@ -1,48 +1,48 @@
 #include "main.h"
 /**
  * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
+ * @frmt: Formatted string in which to print the arguments.
+ * @lst: List of arguments to be printed.
+ * @index: index.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates active flags
- * @width: get width.
+ * @flgs: Calculates active flags
+ * @wid: get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
  */
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-	int flags, int width, int precision, int size)
+int hand_pr(const char *frmt, int *index, va_list lst, char buffer[],
+	int flgs, int wid, int pre, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
-	fmt_t fmt_types[] = {
+	fmt_t frmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	for (i = 0; frmt_types[i].fmt != '\0'; i++)
+		if (frmt[*index] == frmt_types[i].fmt)
+			return (frmt_types[i].fn(lst, buffer, flgs, wid, pre, size));
 
-	if (fmt_types[i].fmt == '\0')
+	if (frmt_types[i].fmt == '\0')
 	{
-		if (fmt[*ind] == '\0')
+		if (frmt[*index] == '\0')
 			return (-1);
 		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
+		if (frmt[*index - 1] == ' ')
 			unknow_len += write(1, " ", 1);
-		else if (width)
+		else if (wid)
 		{
-			--(*ind);
-			while (fmt[*ind] != ' ' && fmt[*ind] != '%')
-				--(*ind);
-			if (fmt[*ind] == ' ')
-				--(*ind);
+			--(*index);
+			while (frmt[*index] != ' ' && frmt[*index] != '%')
+				--(*index);
+			if (frmt[*index] == ' ')
+				--(*index);
 			return (1);
 		}
-		unknow_len += write(1, &fmt[*ind], 1);
+		unknow_len += write(1, &frmt[*index], 1);
 		return (unknow_len);
 	}
 	return (printed_chars);
--- main.h
+++ main.h
@@ -34,14 +34,14 @@ struct fmt
 /**
  * typedef struct fmt fmt_t - Struct op
  *
- * @fmt: The format.
+ * @frmt: The format.
  * @fm_t: The function associated.
  */
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
+int hand_pr(const char *frmt, int *index, va_list lst, char buffer[],
+	int flgs, int wid, int pre, int size);
 
 /****************** FUNCTIONS ******************/
 
